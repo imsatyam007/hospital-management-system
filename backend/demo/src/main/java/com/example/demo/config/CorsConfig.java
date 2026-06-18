8644863f3f2fd3217660ca3f363b5ec0642bb32d
imsatyam007 <iamsatyamc@gmail.com>
2026-06-18 20:17:09 +0530

    Add Vercel URL to CORS allowed origins

--- backend/demo/src/main/java/com/example/demo/config/CorsConfig.java
+++ backend/demo/src/main/java/com/example/demo/config/CorsConfig.java
@@ -15,13 +15,12 @@ public class CorsConfig {
         return new WebMvcConfigurer() {
 
             @Override
-            public void addCorsMappings(
-                    CorsRegistry registry
-            ) {
+            public void addCorsMappings(CorsRegistry registry) {
 
                 registry.addMapping("/**")
                         .allowedOrigins(
-                                "http://localhost:5173"
+                                "http://localhost:5173",
+                                "https://hospital-management-system-imsatyam007s-projects.vercel.app"
                         )
                         .allowedMethods("*")
                         .allowedHeaders("*");
