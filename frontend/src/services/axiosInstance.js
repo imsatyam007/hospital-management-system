28e67f8ef9024ab35d9ff37d16bffea03ae775fe
imsatyam007 <iamsatyamc@gmail.com>
2026-06-18 20:26:08 +0530

    Fix Authorization header template literal

--- frontend/src/services/axiosInstance.js
+++ frontend/src/services/axiosInstance.js
@@ -6,7 +6,7 @@ axiosInstance.interceptors.request.use(
   (config) => {
     const token = localStorage.getItem("token");
     if (token) {
-      config.headers.Authorization = Bearer ${token};
+      config.headers.Authorization = `Bearer ${token}`;
     }
     return config;
   }
