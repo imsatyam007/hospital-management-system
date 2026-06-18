392c2641d22f3bc900e2ef28a38033681960999f
imsatyam007 <iamsatyamc@gmail.com>
2026-06-19 00:48:05 +0530

    Hardcode Render API URL

--- frontend/src/services/axiosInstance.js
+++ frontend/src/services/axiosInstance.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 const axiosInstance = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || "http://localhost:8080",
+  baseURL: "https://hospital-management-system-y2sv.onrender.com",
 });
 axiosInstance.interceptors.request.use(
   (config) => {
@@ -22,5 +22,6 @@ axiosInstance.interceptors.response.use(
   }
 );
 export default axiosInstance;
+
 
 
