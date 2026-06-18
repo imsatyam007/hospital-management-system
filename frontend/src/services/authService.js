bc3c4c6e6323da6c353e8192c0f66affefa76bca
imsatyam007 <iamsatyamc@gmail.com>
2026-06-19 01:15:57 +0530

    Fix authService to use Render URL

--- frontend/src/services/authService.js
+++ frontend/src/services/authService.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API_URL =
-  "http://localhost:8080/api/auth";
+  "https://hospital-management-system-y2sv.onrender.com/api/auth";
 
 export const login = async (username, password) => {
 
